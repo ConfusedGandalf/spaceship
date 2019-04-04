5d38d042847f98f0d2d7c3d2e1f2acb91e7a5edd
ConfusedGandalf <49254050+ConfusedGandalf@users.noreply.github.com>
2019-04-04 18:00:25 +0200

    Add files via upload

--- spaceship.js
+++ spaceship.js
@@ -3,20 +3,23 @@ var angle = 0;
 var directionangle = 0;
 var posX = 230;
 var posY = 370;
+var speed = 1;
 
 function Game(){
     window.addEventListener("keyup", function(e){
-        e.keyCode = false;
+        if (e.keyCode == 38) { speed = 1;}
+        directionangle = 0;
     });
     window.addEventListener("keydown", function(e){
+        if (e.keyCode == 38) { /* up */
+            speed = 4;
+        }
         if (e.keyCode == 37) { /* left */
             directionangle = -1;
-            turnship();
         }
         if (e.keyCode == 39) { /* right */
             directionangle = 1;
-            turnship();
-    }
+        }
     });
     var rebuilder = setInterval(rebuildcanvas, 10);
 }
@@ -27,15 +30,21 @@ function turnship() {
 }
 
 function rebuildcanvas() {
+    turnship();
     angle += directionangle * (Math.PI / 180);
-    posX += 1 * Math.sin(angle);
-    posY -= 1 * Math.cos(angle);
-    spaceship.style.left = posX + 'px';
-    spaceship.style.top = posY + 'px';
+    xDir = speed * Math.sin(angle);
+    yDir = speed * Math.cos(angle);
+    if(posX + xDir > 30 && posX + xDir < 550) {
+        posX += xDir;
+        spaceship.style.left = posX + 'px';
+    }
+    if(posY - yDir > 0 && posY - yDir < 520) {
+        posY -= yDir
+        spaceship.style.top = posY + 'px';
+    }
+    console.log("speed " + speed);
     console.log("angle " + angle);
     console.log("directionangle " + directionangle);
     console.log("posX " + posX);
-    console.log("posY " + posY);
-    directionangle = 0;
-    
+    console.log("posY " + posY);    
 }
